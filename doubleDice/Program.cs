b2dcd1c75b7dd35087d2207fb1c804d3e3dbc90f
grzeszut <grzeszut@LCE47133>
2021-11-29 11:23:04 +0100

    Random dice of two digits

---
+++ doubleDice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace doubleDice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RandomDice dice = new RandomDice();
+			dice.Dice();
+		}
+	}
+}
---
+++ doubleDice/RandomDice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace doubleDice
+{
+	class RandomDice
+	{
+		Random randomDice1 = new Random();
+		int dice1, dice2;
+		int counter = 0;
+		public void Dice()
+		{
+			do
+			{
+				dice1 = randomDice1.Next(1, 6);
+				Thread.Sleep(1000);
+				dice2 = randomDice1.Next(1, 6);
+				Console.WriteLine($"First dice: {dice1}\nSecond dice: {dice2}\n");
+				counter++;
+			}
+			while (dice1 != dice2);
+			Console.WriteLine($"First dice = second dice, after {counter} attempts");
+		}
+
+	}
+}
